7e177645906c6257f0e603d3ac8df2ab1fe65862
rishirhyspavan <surisettypavankumar999@gmail.com>
2021-10-22 14:16:25 +0530

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
+
+function App(props) {
+  console.log(props);
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
